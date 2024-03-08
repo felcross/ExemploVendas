ecd505e7fc440ca5f54b6fe5a76cf3a2be27a7e6
Felipe Batista <feljava32@gmail.com>
2024-03-08 13:13:21 -0300

    primeeiro teste

---
+++ src/main/java/br/com/domain/Cliente.java
@@ -0,0 +1,5 @@
+package br.com.domain;
+
+public class Cliente {
+
+}
---
+++ src/test/java/br/com/teste/ClienteTeste.java
@@ -0,0 +1,20 @@
+package br.com.teste;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import br.com.domain.Cliente;
+
+
+public class ClienteTeste {
+
+	@Test
+	public void pesquisarCliente() {
+		Cliente cli = new Cliente();
+		     
+		  cli.pesquisarCli;
+	}
+	
+
+	
+}
