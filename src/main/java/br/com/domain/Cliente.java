a9167d0dbea9a2b30f501e09e835c2a1f24965e2
Felipe Batista <feljava32@gmail.com>
2024-04-19 17:00:53 -0300

    vendas teste

--- src/main/java/br/com/domain/Cliente.java
+++ src/main/java/br/com/domain/Cliente.java
@@ -2,89 +2,36 @@ package br.com.domain;
 
 public class Cliente {
 
-	private String nome;
-	private Long cpf;
-	private Long tel;
-	private String end;
-	private Integer numero;
-	private String cidade;
-	private String estado;
-	
-	public Cliente(String nome, Long cpf, Long tel, String end, Integer numero, String cidade, String estado) {
-		this.nome = nome;
-		this.cpf = cpf;
-		this.tel = tel;
-		this.end = end;
-		this.numero = numero;
-		this.cidade = cidade;
-		this.estado = estado;
-	}
-	
-	public Cliente() {
-	}
 
+	private Long id;
+	private String codigo;
+	private String nome;
+	
+	
+	public Cliente(Long id, String codigo, String nome) {
+		this.id = id;
+		this.codigo = codigo;
+		this.nome = nome;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
 	public String getNome() {
 		return nome;
 	}
-
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
-	public Long getCpf() {
-		return cpf;
-	}
-
-	public void setCpf(Long cpf) {
-		this.cpf = cpf;
-	}
-
-	public Long getTel() {
-		return tel;
-	}
-
-	public void setTel(Long tel) {
-		this.tel = tel;
-	}
-
-	public String getEnd() {
-		return end;
-	}
-
-	public void setEnd(String end) {
-		this.end = end;
-	}
-
-	public Integer getNumero() {
-		return numero;
-	}
-
-	public void setNumero(Integer numero) {
-		this.numero = numero;
-	}
-
-	public String getCidade() {
-		return cidade;
-	}
-
-	public void setCidade(String cidade) {
-		this.cidade = cidade;
-	}
-
-	public String getEstado() {
-		return estado;
-	}
-
-	public void setEstado(String estado) {
-		this.estado = estado;
-	}
-
-	@Override
-	public String toString() {
-		return "Cliente [nome=" + nome + ", cpf=" + cpf + ", tel=" + tel + ", end=" + end + ", numero=" + numero
-				+ ", cidade=" + cidade + ", estado=" + estado + "]";
-	}
 	
-		
+	
 	
 }
